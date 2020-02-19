47004d60decccbaf7039c9c199db89160b347bd0
shanto datta <shanto datta@DESKTOP-UUTHF3G>
2020-02-20 02:12:57 +0600

    student class

---
+++ Student/src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+
+	public static void main(String[] args) {
+Student s1=new Student();
+		
+		s1.setId(100);
+		s1.setName("Shanto");
+		
+		System.out.println(s1);
+		
+		Student s2=new Student();
+		s2.setId(200);
+		s2.setName("Abir");
+		
+		System.out.println(s2);
+		
+		Student s3=new Student();
+		s3.setName("Nirob");
+		s3.setId(300);
+	    System.out.println(s3);
+
+	}
+}
